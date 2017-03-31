57bc7e947ce7bf8a38bf12fe18451a868cfb2d02
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-03-31 08:53:17 +0300

    initial commit

---
+++ LogWriter.Service/LogMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace Cmas.Services.LogWriter
+{
+    /// <summary>
+    /// Сообщение для логирования
+    /// </summary>
+    public class LogMessage
+    {
+        public String Message = String.Empty;
+        public LogLevel Level = LogLevel.None;
+    }
+}
---
+++ Web/Program.cs
@@ -0,0 +1,21 @@
+﻿using Cmas.Infrastructure.ServicesStartup;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseKestrel()
+                //.UseIISIntegration()
+                .UseStartup<ServicesStartup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
