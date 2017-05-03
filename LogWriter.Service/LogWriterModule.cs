df091175cdf41b5331f9ad7160ad6511f0384d5e
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-05-03 10:25:01 +0300

    refactoring

--- LogWriter.Service/LogWriterModule.cs
+++ LogWriter.Service/LogWriterModule.cs
@@ -1,7 +1,11 @@
 ﻿using System;
-using Microsoft.Extensions.Logging;
 using Nancy;
 using Nancy.ModelBinding;
+using Nancy.Responses.Negotiation;
+using System.Threading.Tasks;
+using System.Threading;
+using Nancy.Validation;
+using Cmas.Infrastructure.ErrorHandler;
 
 namespace Cmas.Services.LogWriter
 {
@@ -10,19 +14,33 @@ namespace Cmas.Services.LogWriter
     /// </summary>
     public class LogWriterModule : NancyModule
     {
-        private ILogger _logger;
+        private LogWriterService _logWriterService;
 
-        public LogWriterModule(ILoggerFactory loggerFactory)
+        public LogWriterModule(IServiceProvider serviceProvider)
         {
-            _logger = loggerFactory.CreateLogger<LogWriterModule>();
+            _logWriterService = new LogWriterService(serviceProvider);
 
-            Post("log", args =>
-            {
-                LogMessage logMessage = this.Bind<LogMessage>();
-                _logger.Log(logMessage.Level, (EventId) 0, logMessage.Message, (Exception) null, (state, error) => state.ToString());
-
-                return HttpStatusCode.OK;
-            });
+            Post<Negotiator>("log", LogMessageHandlerAsync);
         }
+
+        #region Обработчики
+
+        private async Task<Negotiator> LogMessageHandlerAsync(dynamic args, CancellationToken token)
+        {
+            LogMessage request = this.Bind<LogMessage>();
+
+            var validationResult = this.Validate(request);
+
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationErrorException(validationResult.FormattedErrors);
+            }
+
+            _logWriterService.Log(request);
+
+            return Negotiate.WithStatusCode(HttpStatusCode.OK);
+        }
+
+        #endregion
     }
 }
---
+++ LogWriter.Service/LogWriterService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace Cmas.Services.LogWriter
+{
+    public class LogWriterService
+    {
+        private ILogger _logger;
+
+        public LogWriterService(IServiceProvider serviceProvider)
+        {
+            var _loggerFactory = (ILoggerFactory)serviceProvider.GetService(typeof(ILoggerFactory));
+
+            _logger = _loggerFactory.CreateLogger<LogWriterService>();
+        }
+
+        public void Log(LogMessage logMessage)
+        {
+            _logger.Log(logMessage.Level, (EventId)0, logMessage.Message, (Exception)null, (state, error) => state.ToString());
+        }
+    }
+
+}
